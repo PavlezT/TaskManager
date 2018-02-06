1d1538ab41813c287f42d1c8b5f804aec3b46f29
Aleksandr Demianenko <aleksandr.demianenko@lizardsoftdev.onmicrosoft.com>
2018-02-06 13:56:09 +0200

    Codestyle

--- Server/controllers/user.js
+++ Server/controllers/user.js
@@ -2,41 +2,41 @@ const userModel = require('../models/user');
 
 exports.current = (req, res) => {
     userModel.current(req.session.user)
-        .then(user=>{//([company,user]
+        .then(user => {//([company,user]
             res.send(user);
         })
         .catch(err => {
             loggerHelper.logger.log('error', 'Query error: %s', err);
             res.sendStatus(500);
-        })
+        });
 };
 
-exports.userCompany = (req,res) => {
+exports.userCompany = (req, res) => {
      userModel.getCompanyById(req.session.user.company)
-        .then( company => {
+        .then(company => {
             res.send(company);
         })
-        .catch( error => {
+        .catch(error => {
             loggerHelper.logger.log('error', 'Query error: %s', err);
             res.sendStatus(500);
-        })
+        });
 }
 
-exports.ensureCompany = (req,companyInfo,userInfo) =>{
+exports.ensureCompany = (req, companyInfo, userInfo) =>{
     return userModel.getCompany(companyInfo)
-        .then(({company,created}) => {
-            userInfo.company = { "_col": "Companies", "_docId": company["_id"].toString() };
-            return {userInfo : userInfo, created : created};
-        })
+        .then(({company, created}) => {
+            userInfo.company = {"_col": "Companies", "_docId": company["_id"].toString()};
+            return {userInfo: userInfo, created: created};
+        });
 }
 
 exports.ensureUser = (req, companyCreated, userInfo) => {
-    return userModel.ensureUser(userInfo,companyCreated)
+    return userModel.ensureUser(userInfo, companyCreated)
         .then(({user, created}) => {
             req.session.user.company = userInfo.company;
             req.session.user.userId = user["_id"];
             req.session.user.admin = user.Admin;
-            return {ensuredUser : user, wasCreated : created};
+            return {ensuredUser: user, wasCreated: created};
         });
 }
 
@@ -49,5 +49,5 @@ exports.createCompanyCollections = (data) => {
     promiseArr.push(userModel.createCollection(companyId+"UserTaskCategories"));
     promiseArr.push(userModel.createCollection(companyId+"TaskDiscussions"));  
 
-    return Promise.all(promiseArr).then(() => { return data;})
+    return Promise.all(promiseArr).then(() => { return data; })
 }
